fc5045a23164d5c6f3d4ebe0a74797e768d1fbaa
jason00111 <emberleyjason@gmail.com>
2017-06-19 15:59:25 -0700

    removes shopper table

--- part-2/schema.sql
+++ part-2/schema.sql
@@ -1,7 +1,6 @@
-DROP TABLE IF EXISTS item;
-DROP TABLE IF EXISTS shopper;
-DROP TABLE IF EXISTS purchase;
 DROP TABLE IF EXISTS item_purchase;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS purchase;
 
 CREATE TABLE item (
   id serial PRIMARY KEY,
@@ -10,14 +9,9 @@ CREATE TABLE item (
   section varchar(40)
 );
 
-CREATE TABLE shopper (
-  id serial PRIMARY KEY,
-  name varchar(40)
-);
-
 CREATE TABLE purchase (
   id serial PRIMARY KEY,
-  shopper_id int REFERENCES shopper,
+  shopper varchar(40),
   creation_date timestamp
 );
 
