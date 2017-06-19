843aa9788adda82d80d39e25d784df100e08974a
jason00111 <emberleyjason@gmail.com>
2017-06-19 15:30:34 -0700

    fixes database tests and makes them asynchonous

--- part-2/database_test.js
+++ part-2/database_test.js
@@ -4,16 +4,31 @@ const expect = chai.expect
 
 const { itemsInSection, cheapItems, countItemsInSection } = require('./database.js')
 
-it('itemsInSection("bulk") should return "Flour", "Pasta", and "Rice"', () => {
-  expect(itemsInSection('bulk')).to.deep.equal(['Flour', 'Pasta', 'Rice'])
-})
+it('itemsInSection("bulk") should return "Flour", "Pasta", and "Rice"',
+  done => {
+    itemsInSection('bulk').then(results => {
+      const names = results.map(result => result.name)
+      expect(names).to.include('Flour')
+      expect(names).to.include('Pasta')
+      expect(names).to.include('Rice')
+      done()
+    })
+  }
+)
 
-it('cheapItems() should return "Fish" as the first item and "Honey" as the last item', () => {
-  const result = cheapItems()
-  expect(result[0]).to.equal('Fish')
-  expect(result[result.length - 1]).to.equal('Honey')
-})
+it('cheapItems() should return "Fish" as the first item and "Honey" as the last item',
+  done => {
+    cheapItems().then(results => {
+      expect(results[0].price).to.equal('$0.49')
+      expect(results[results.length - 1].price).to.equal('$9.31')
+      done()
+    })
+  }
+)
 
-it('countItemsInSection() should return 5', () => {
-  expect(countItemsInSection()).to.equal(5)
+it('countItemsInSection() should return 5', done => {
+  countItemsInSection().then(result => {
+    expect(result).to.equal(5)
+    done()
+  })
 })
