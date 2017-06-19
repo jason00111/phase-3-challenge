7c039703c5866c40ee1ab8a178d2e7eedfc349e7
jason00111 <emberleyjason@gmail.com>
2017-06-19 12:30:53 -0700

    refactors schema to put section in item table

--- part-2/schema.sql
+++ part-2/schema.sql
@@ -1,19 +1,13 @@
 DROP TABLE IF EXISTS item;
-DROP TABLE IF EXISTS section;
 DROP TABLE IF EXISTS shopper;
 DROP TABLE IF EXISTS purchase;
-DROP TABLE IF EXISTS item_section;
 DROP TABLE IF EXISTS item_purchase;
 
 CREATE TABLE item (
   id serial PRIMARY KEY,
   name varchar(40),
-  price money
-);
-
-CREATE TABLE section (
-  id serial PRIMARY KEY,
-  name varchar(40)
+  price money,
+  section varchar(40)
 );
 
 CREATE TABLE shopper (
@@ -25,11 +19,6 @@ CREATE TABLE purchase (
   id serial PRIMARY KEY,
   shopper_id int REFERENCES shopper,
   creation_date timestamp
-);
-
-CREATE TABLE item_section (
-  item_id int REFERENCES item,
-  section_id int REFERENCES section
 );
 
 CREATE TABLE item_purchase (
