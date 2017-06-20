58146f4aafb6a5b46f6c3281e472554ad322e763
jason00111 <emberleyjason@gmail.com>
2017-06-19 17:07:10 -0700

    adds seed data for purchases

--- part-2/load-data.sql
+++ part-2/load-data.sql
@@ -1,1 +1,17 @@
 COPY item (name, price, section) FROM :csvPath DELIMITERS ',' CSV HEADER;
+
+INSERT INTO purchase (shopper) VALUES ('Peter'), ('Tony'), ('Steve');
+
+INSERT INTO item_purchase (item_id, purchase_id)
+VALUES (1, 1),
+(2, 1),
+(5, 1),
+(6, 1),
+(14, 1),
+(2, 2),
+(5, 2),
+(9, 2),
+(9, 2),
+(1, 3),
+(12, 3),
+(4, 3);
