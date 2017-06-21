6392780d341ff27dff0500446423416752d91b90
jason00111 <emberleyjason@gmail.com>
2017-06-21 15:22:31 -0700

    normalizes schema

--- part-2/schema.sql
+++ part-2/schema.sql
@@ -1,17 +1,34 @@
 DROP TABLE IF EXISTS item_purchase;
-DROP TABLE IF EXISTS item;
 DROP TABLE IF EXISTS purchase;
+DROP TABLE IF EXISTS shopper;
+DROP TABLE IF EXISTS item_section;
+DROP TABLE IF EXISTS section;
+DROP TABLE IF EXISTS item;
 
 CREATE TABLE item (
   id serial PRIMARY KEY,
   name varchar(40),
-  price money,
-  section varchar(40)
+  price money
+);
+
+CREATE TABLE section (
+  id serial PRIMARY KEY,
+  name varchar(40)
+);
+
+CREATE TABLE item_section (
+  item_id int REFERENCES item,
+  section_id int REFERENCES section
+);
+
+CREATE TABLE shopper (
+  id serial PRIMARY KEY,
+  name varchar(40)
 );
 
 CREATE TABLE purchase (
   id serial PRIMARY KEY,
-  shopper varchar(40),
+  shopper_id int REFERENCES shopper,
   creation_date timestamp DEFAULT now()
 );
 
