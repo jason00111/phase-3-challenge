bb091e8807ccb20ae1df7f7bd48e24112f7bb932
jason00111 <emberleyjason@gmail.com>
2017-06-20 18:10:34 -0700

    adds clear cart functionality

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -4,9 +4,15 @@ const cartModal = document.getElementById('cart-modal')
 
 const modalCloseButton = document.getElementById('modal-close-button')
 
-const addToCartButtons = document.querySelectorAll('.item button')
+const clearCartButton = document.getElementById('clear-cart-button')
 
 const itemCountSpan = document.getElementById('cart-item-count')
+
+const cartItemSection = document.querySelectorAll('#cart-modal .item-section')[0]
+
+const cartTotalSpan = document.getElementById('cart-total')
+
+const addToCartButtons = document.querySelectorAll('.item button')
 
 cartButton.addEventListener('click', () =>
   cartModal.style.visibility = 'visible'
@@ -15,3 +21,10 @@ cartButton.addEventListener('click', () =>
 modalCloseButton.addEventListener('click', () =>
   cartModal.style.visibility = 'hidden'
 )
+
+clearCartButton.addEventListener('click', () => {
+  itemCountSpan.textContent = '0'
+  while (cartItemSection.firstChild)
+    cartItemSection.firstChild.remove()
+  cartTotalSpan.textContent = '0'
+})
