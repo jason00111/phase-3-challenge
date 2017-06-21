cde74fb952a061d54aab6c426b1759e8ae1f5759
jason00111 <emberleyjason@gmail.com>
2017-06-21 15:23:16 -0700

    fixes seed data script for new schema

--- part-2/load-data.sql
+++ part-2/load-data.sql
@@ -1,17 +1,40 @@
-COPY item (name, price, section) FROM :csvPath DELIMITERS ',' CSV HEADER;
+DROP TABLE IF EXISTS temp;
 
-INSERT INTO purchase (shopper) VALUES ('Peter'), ('Tony'), ('Steve');
+CREATE TABLE temp (
+  name varchar(40),
+  price money,
+  section varchar(40)
+);
+
+COPY temp (name, price, section) FROM :csvPath DELIMITERS ',' CSV HEADER;
+
+INSERT INTO section (name)
+SELECT DISTINCT section FROM temp;
+
+INSERT INTO item (name, price)
+SELECT name, price FROM temp;
+
+INSERT INTO item_section
+SELECT item.id AS item_id, section.id AS section_id FROM temp
+JOIN section ON section.name = temp.section
+JOIN item ON item.name = temp.name;
+
+DROP TABLE temp;
+
+INSERT INTO shopper (name) VALUES ('Peter'), ('Tony'), ('Steve');
+
+INSERT INTO purchase (shopper_id) VALUES (1), (2), (3);
 
 INSERT INTO item_purchase (item_id, purchase_id)
-VALUES (1, 1),
-(2, 1),
-(5, 1),
-(6, 1),
-(14, 1),
-(2, 2),
-(5, 2),
-(9, 2),
-(9, 2),
-(1, 3),
-(12, 3),
-(4, 3);
+VALUES  (1, 1),
+        (2, 1),
+        (5, 1),
+        (6, 1),
+        (14, 1),
+        (2, 2),
+        (5, 2),
+        (9, 2),
+        (9, 2),
+        (1, 3),
+        (12, 3),
+        (4, 3);
