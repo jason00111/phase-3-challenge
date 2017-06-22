d659d52d8a22f15b279b38629a3e9f2e6eba6042
jason00111 <emberleyjason@gmail.com>
2017-06-21 18:10:41 -0700

    fixes tests so they fail properly

--- part-2/database_test.js
+++ part-2/database_test.js
@@ -13,24 +13,18 @@ const {
 } = require('./database.js')
 
 it('allItems() should return all the itms\' id\'s, names, prices, and sections',
-  done => {
+  () =>
     allItems().then(results => {
       expect(results).to.be.an('array').with.lengthOf(41)
       expect(results[0]).to.have.property('id')
       expect(results[0]).to.have.property('name')
       expect(results[0]).to.have.property('price')
       expect(results[0]).to.have.property('section')
-      done()
     })
-    .catch(err => {
-      console.error(err)
-      done()
-    })
-  }
 )
 
 it('itemsInSection("bulk") should return "Flour", "Pasta", and "Rice"',
-  done => {
+  () =>
     itemsInSection('bulk').then(results => {
       expect(results).to.be.an('array').with.lengthOf(3)
       expect(results[0]).to.have.property('id')
@@ -40,17 +34,11 @@ it('itemsInSection("bulk") should return "Flour", "Pasta", and "Rice"',
       expect(names).to.include('Flour')
       expect(names).to.include('Pasta')
       expect(names).to.include('Rice')
-      done()
     })
-    .catch(err => {
-      console.error(err)
-      done()
-    })
-  }
 )
 
 it('cheapItems() should return "Fish" as the first item and "Honey" as the last item',
-  done => {
+  () =>
     cheapItems().then(results => {
       expect(results).to.be.an('array').with.lengthOf(25)
       expect(results[0]).to.have.property('id')
@@ -58,60 +46,32 @@ it('cheapItems() should return "Fish" as the first item and "Honey" as the last 
 
       expect(results[0].price).to.equal('$0.49')
       expect(results[results.length - 1].price).to.equal('$9.31')
-      done()
     })
-    .catch(err => {
-      console.error(err)
-      done()
-    })
-  }
 )
 
-it('countItemsInSection("packaged") should return 5', done => {
+it('countItemsInSection("packaged") should return 5', () =>
   countItemsInSection('packaged').then(result => {
     expect(result).to.be.a('string')
     expect(result).to.equal('5')
-    done()
   })
-  .catch(err => {
-    console.error(err)
-    done()
-  })
-})
+)
 
-it('mostRecentOrders() finds the id\'s and order dates for the 10 most recent orders', done => {
+it('mostRecentOrders() finds the id\'s and order dates for the 10 most recent orders', () =>
   mostRecentOrders().then(results => {
     expect(results).to.be.an('array').with.lengthOf(3)
-    done()
   })
-  .catch(err => {
-    console.error(err)
-    done()
-  })
-})
+)
 
-it('lastShopperName() finds the shopper\'s name who made the most recent order', done => {
+it('lastShopperName() finds the shopper\'s name who made the most recent order', () =>
   lastShopperName().then(result => {
     expect(result).to.be.a('string')
     expect(result).to.equal('Steve')
-    done()
   })
-  .catch(err => {
-    console.error(err)
-    done()
-  })
-})
+)
 
-it('orderTotal(orderId) finds the total cost of an order', () => {
+it('orderTotal(orderId) finds the total cost of an order', () =>
   orderTotal(3).then(result => {
-    console.log('hello?')
-    console.log('hello?')
     expect(result).to.be.a('string')
     expect(result).to.equal('$35.62')
-    done()
   })
-  .catch(err => {
-    console.error(err)
-    done()
-  })
-})
+)
